61bf92f820718426e04f35be5be53ceab5f4b806
a.harada <redhornet96@gmail.com>
2016-04-27 12:47:29 +0900

    modify

--- main.rb
+++ main.rb
@@ -1,10 +1,48 @@
-require 'sinatra'
-#require './lib/twilio_caller'
 require 'dotenv'
+require 'sinatra'
+require 'line/bot/client'
 
 Dotenv.load
 
 get '/' do
   'hoge'
-  #erb :index
+end
+
+post '/' do
+  client = Line::Bot::Client.new do |config|
+    config.channel_id     = ENV['LINE_CHANNEL_ID']
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_mid    = ENV["LINE_CHANNEL_MID"]
+  end
+
+  params = JSON.parse(request.body.read)
+  params['result'].each do |message|
+    line_user_id = message['content']['from']
+  end
+
+  client.send_text([line_user_id], text: "Hello LINE!")
+
+
+  #params = JSON.parse(request.body.read)
+  #
+  # params['result'].each do |msg|
+  #   request_content = {
+  #     to: [msg['content']['from']],
+  #     toChannel: 1383378250, # Fixed  value
+  #     eventType: "138311608800106203", # Fixed value
+  #     content: msg['content']
+  #   }
+  #
+  # endpoint = 'https://trialbot-api.line.me/v1/events'
+  #
+  #   RestClient.proxy = ENV["FIXIE_URL"]
+  #   RestClient.post(endpoint_uri, content_json, {
+  #     'Content-Type' => 'application/json; charset=UTF-8',
+  #     'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+  #     'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+  #     'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+  #   })
+  # end
+  #
+  # "OK"
 end
